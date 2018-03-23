cada5967dff3247b420d477998656afdc7d30d71
marcialwushu <marcialwushu@gmail.com>
2018-03-23 14:38:02 -0300

    initial commit - wp rest api with ionic3 CRUD

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the ContactPage page.
+ * Generated class for the QuotesPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html',
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
 })
-export class ContactPage {
+export class QuotesPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactPage');
+    console.log('ionViewDidLoad QuotesPage');
   }
 
 }
